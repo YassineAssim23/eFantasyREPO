4e354344bbcd65a76ba3b4379a2efee0a1d138dd
YassineAssim23 <assimy@sheridancollege.ca>
2024-10-09 23:10:45 -0400

    setting up backend. basic toml setup and creation of source folders

--- eFantasyBackend/src/main.rs
+++ eFantasyBackend/src/main.rs
@@ -1,5 +1,23 @@
-fn main() {
-    let mut x = 4;
-    println!("x is {}", x);
-    let x = 5;
+mod models;
+mod handlers;
+mod db;
+
+use actix_web::{HttpServer, App, web::Data};
+use dotenv::dotenv;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    
+    println!("Starting running at http://localhost:8080");
+
+    HttpServer::new(move || {
+        App::new()
+            .route("/", web::get().to(|| async {HttpResponse::Ok().body("EHLIE???") }))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
+
+
