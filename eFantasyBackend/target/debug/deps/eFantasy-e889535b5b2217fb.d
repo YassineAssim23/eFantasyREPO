1b7f669c0b346cd2647f065b117ae53e0bcd5d25
Shalin Patel <42554035+PatelShalin@users.noreply.github.com>
2024-10-10 22:38:32 -0400

    remove target folder

--- eFantasyBackend/target/debug/deps/eFantasy-e889535b5b2217fb.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Yassine\Documents\MessingAround\FantasyLOL\eFantasy\target\debug\deps\libeFantasy-e889535b5b2217fb.rmeta: src/main.rs
-
-C:\Users\Yassine\Documents\MessingAround\FantasyLOL\eFantasy\target\debug\deps\eFantasy-e889535b5b2217fb.d: src/main.rs
-
-src/main.rs:
--- eFantasyBackend/target/debug/deps/eFantasy.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Yassine\Documents\MessingAround\eFantasyProject\eFantasyREPO\eFantasyBackend\target\debug\deps\eFantasy.exe: src/main.rs
-
-C:\Users\Yassine\Documents\MessingAround\eFantasyProject\eFantasyREPO\eFantasyBackend\target\debug\deps\eFantasy.d: src/main.rs
-
-src/main.rs:
